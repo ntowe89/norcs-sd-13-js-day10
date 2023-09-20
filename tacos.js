82715f3d0ef5068de379b0d4544ccb313604b644
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2023-09-20 18:33:06 -0500

    tacos

---
+++ tacos.js
@@ -0,0 +1,19 @@
+//if you go to this url it shows the json
+let api_base_url = "https://60d23844858b410017b2d60b.mockapi.io/tacos"
+
+//using fetch go print the tacos by id and by all
+async function getAllTacos(){
+    //not returning data only console.log it
+}
+
+async function getTacosById(){
+//not returning data only console.log it
+}
+
+//use get all, to print all tacos
+getAllTacos()
+
+//use get by id to print taco 3
+getTacosById(3)
+//use get by id to print taco 3789
+getTacosById(3789)
