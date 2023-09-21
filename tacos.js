5b3fb61f1bd0a691471873f97d0cd6d00ae77dd8
Nathan Towe <nathanwtowe89@gmail.com>
2023-09-20 21:44:18 -0400

    example

--- tacos.js
+++ tacos.js
@@ -4,10 +4,28 @@ let api_base_url = "https://60d23844858b410017b2d60b.mockapi.io/tacos"
 //using fetch go print the tacos by id and by all
 async function getAllTacos(){
     //not returning data only console.log it
+    let response = await fetch(api_base_url)
+
+    console.log("response from api",response)
+
+    let taco = await response.json()
+    console.log(taco)
+
+    
 }
 
-async function getTacosById(){
+async function getTacosById(tacoId){
 //not returning data only console.log it
+   
+    try{
+        let response = await fetch(api_base_url+"/"+tacoId)
+   console.log(response)
+    }
+    catch(error){
+        console.error("could handle better")
+    }
+
+
 }
 
 //use get all, to print all tacos
